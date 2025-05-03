2969980af18d61e51ef18c1f328dec7c6e171a59
rakimsSpiritual <spiritualrakims@gmail.com>
2025-05-03 10:40:00 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,27 +1,23 @@
-// src/lib/auth.ts or lucia.ts
-
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { Lucia } from "lucia";
 
 const client = new PrismaClient();
 
-const adapter = new PrismaAdapter(
-  client.user,
-  client.session,
-  client.key
-);
+const adapter = new PrismaAdapter({
+  user: client.user,
+  session: client.session,
+  key: client.key // Ensure you have this model in your Prisma schema
+});
 
 export const auth = new Lucia(adapter, {
   env: process.env.NODE_ENV === "production" ? "PRODUCTION" : "DEV",
-  getUserAttributes: (data) => {
-    return {
-      id: data.id,
-      username: data.username,
-      displayName: data.displayName,
-      avatarUrl: data.avatarUrl
-    };
-  }
+  getUserAttributes: (data) => ({
+    id: data.id,
+    username: data.username,
+    displayName: data.displayName,
+    avatarUrl: data.avatarUrl
+  })
 });
 
 export type Auth = typeof auth;
