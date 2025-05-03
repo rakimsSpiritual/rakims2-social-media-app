690191ef12643737250e0b8deb028683986e12c0
rakimsSpiritual <spiritualrakims@gmail.com>
2025-05-03 10:34:17 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,27 @@
-import { Lucia } from "lucia";
-import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+// src/lib/auth.ts or lucia.ts
+
 import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia } from "lucia";
 
 const client = new PrismaClient();
 
-const adapter = new PrismaAdapter(client, {
-  user: "users",
-  key: "keys",
-  session: "sessions"
-});
+const adapter = new PrismaAdapter(
+  client.user,
+  client.session,
+  client.key
+);
 
-export const auth = new (class extends Lucia {
-  constructor() {
-    super(adapter, {
-      env: process.env.NODE_ENV === "production" ? "PRODUCTION" : "DEV"
-    });
-  }
-
-  getUserAttributes(user: any) {
+export const auth = new Lucia(adapter, {
+  env: process.env.NODE_ENV === "production" ? "PRODUCTION" : "DEV",
+  getUserAttributes: (data) => {
     return {
-      username: user.username,
-      email: user.email,
-      displayName: user.displayName,
-      avatarUrl: user.avatarUrl
+      id: data.id,
+      username: data.username,
+      displayName: data.displayName,
+      avatarUrl: data.avatarUrl
     };
   }
-})();
+});
+
+export type Auth = typeof auth;
